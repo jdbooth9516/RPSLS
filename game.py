5c885a766d646bd522083f6033917bcc7a171786
Jeremiah Booth <jeremiah.boothphotog@gmail.com>
2021-05-18 08:57:12 -0700

    final ingame insturctions

--- game.py
+++ game.py
@@ -42,11 +42,16 @@ class Game:
         time.sleep(2)
         print("Rules are simply just like Rock, Paper, Scissors. Just with the addition of 2 more options of lizard and spock.")
         time.sleep(2)
+        print("Rock crushes Scissors, Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard, Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard, Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock")
+        print()
+        time.sleep(3)
+        print('Got it, good')
+        time.sleep(2)
         print()
         print('For the number of players enter 1 to play against the AI, enter 2 to play against another human')
         time.sleep(2)
         print()
-        print('when choosing a gesture hit a single number and hit enter the number that you have selected will not '
+        print('When choosing a gesture hit a single number and hit enter the number that you have selected will not '
               'be displayed in terminal.')
         print()
         valid_1 = False
